9b59da78d8f668d6184512cefa869156a8a69480
Rodrigo Dlugokenski <rddweb@gmail.com>
2012-11-07 10:42:25 -0200

    Adicionado o controller de sessions novamente.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+      redirect_to root_url, :notice => "Entrou no sistema"
+    else
+      flash.now.alert = "E-mail ou senha incorretos, verifique."
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Saiu do sistema"
+  end
+end
