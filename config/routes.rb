a340fe9b0b82b8b98d7737fd07e4a1bfda4f812e
Rodrigo Dlugokenski <rddweb@gmail.com>
2012-11-10 01:21:05 -0200

    Várias melhorias nas boas vindas e no login Novos truques do bootstrap! Ícone e logotipo do aplicativo em vários tamanhos

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,12 @@ Cifrao::Application.routes.draw do
 
   get "sessions/new"
 
-  get "sessions/create"
-
   get "sessions/destroy"
 
   get "users/new"
 
-  get "users/create"
+  get "users/reset_password"
+
   resources :users
   resources :sessions
 
